eae2f262933053f22da07e561b60c215653720bf
root <root@Morandini.localdomain>
2017-05-27 00:57:48 +0200

    isdigit

---
+++ main.c
@@ -0,0 +1,15 @@
+int _ft_isdigit(int c);
+
+
+main()
+{
+	printf("%d\n", _ft_isdigit('0'));
+	printf("%d\n", _ft_isdigit('1'));
+	printf("%d\n", _ft_isdigit('2'));
+	printf("%d\n", _ft_isdigit('9'));
+	printf("%d\n", _ft_isdigit(':'));
+	printf("%d\n", _ft_isdigit('F'));
+	printf("%d\n", _ft_isdigit('P'));
+	printf("%d\n", _ft_isdigit('.'));
+	printf("%d\n", _ft_isdigit('§'));
+}
